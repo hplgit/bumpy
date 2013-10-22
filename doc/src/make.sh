d00f3b146dc8a5c0315f5707ea493a5d41cd5b25
Hans Petter Langtangen <hpl@simula.no>
2013-10-22 11:42:57 +0200

    updates

--- doc/src/make.sh
+++ doc/src/make.sh
@@ -2,16 +2,16 @@
 name=bumpy
 wrap=$name
 
+# Plain HTML
 doconce format html $wrap
 if [ $? -ne 0 ]; then echo "doconce could not compile document"; exit; fi
 
+# Sphinx
+doconce format sphinx $wrap  # always generate new
 doconce sphinx_dir theme=cbc title="A Worked Example on Scientific Computing with Python" author="H. P. Langtangen" $wrap
 python automake_sphinx.py
 
-doconce format html $wrap
-
-# Copy to hplgit (or maybe just gh-pages?)
-#dest=~/vc/hplgit.github.com/teamods/bumpy/
+# Publish
 dest=../pub
 rm -rf $dest/sphinx
 cp -r sphinx-rootdir/_build/html $dest/sphinx
