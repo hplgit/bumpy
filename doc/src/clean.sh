0bbe75945991077cfa036517b4dc71c0ee511ee9
Hans Petter Langtangen <hpl@simula.no>
2015-08-25 22:06:48 +0200

    updates

--- doc/src/clean.sh
+++ doc/src/clean.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 doconce clean
 rm -rf *.png automake*.py
+rm -rf lectures-bumpy*.pdf lectures-bumpy*.html lectures-basics*.pdf lectures-basics*.html reveal.js deck.js *.log lectures*.tex ipynb*
